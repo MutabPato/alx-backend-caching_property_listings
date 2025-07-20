30aa187026028e4877075568d041b55a2660712c
MutabPato <patrickmtbz@gmail.com>
2025-07-20 16:53:59 +0300

    Fix: error handling

--- properties/utils.py
+++ properties/utils.py
@@ -23,21 +23,19 @@ def get_all_properties():
         return json.loads(data)
 
 def get_redis_cache_metrics():
-    conn = get_redis_connection("default")
-    info = conn.info("stats")
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    total_requests = hits + misses
+    try:
+        conn = get_redis_connection("default")
+        info = conn.info("stats")
+        hits = info.get("keyspace_hits", 0)
+        misses = info.get("keyspace_misses", 0)
+        total_requests = hits + misses
+        hit_ratio = hits / total_requests if total_requests > 0 else 0
 
-    if total_requests > 0:
-        hit_ratio = hits / total_requests
         logger.info(f"'hits': {hits}, 'misses': {misses}, 'hit_ratio': {hit_ratio}")
         return {"hits": hits, "misses": misses, "hit_ratio": hit_ratio}
-    else:
-        hit_ratio = 0
-        logger.error("No requests made to the redis database")
-        return {"hits": hits, "misses": misses, "hit_ratio": hit_ratio}
-
+    except Exception as e:
+        logger.error(f"Error connecting to redis: {e}")
+        raise
 
 
 
